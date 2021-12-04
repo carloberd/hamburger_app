025b20145d13408eb8a49e7de73865cd99b7d822
carloberd <carloberdondini@gmail.com>
2021-12-04 13:00:23 +0100

    Add avatar image, name, status member and account balance

--- lib/header.dart
+++ lib/header.dart
@@ -34,16 +34,59 @@ class _HeaderState extends State<Header> {
                     ),
                   ],
                 ),
-                child: Column(
+                child: Stack(
                   children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: const [
-                        CircleAvatar(
-                          backgroundColor: Colors.white70,
-                          radius: 35,
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundColor: Colors.white70,
+                              radius: 35,
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("images/avatar.jpg"),
+                                radius: 30,
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Column(
+                              children: [
+                                const Text(
+                                  'Carlo Berdondini',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.black54,
+                                  ),
+                                  child: const Text(
+                                    'Premium Member',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            const Text(
+                              '150 €',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
