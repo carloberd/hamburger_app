52922f6d17ea9926d94ead32afcd27e1c193f67c
carloberd <carloberdondini@gmail.com>
2021-12-04 19:18:37 +0100

    Add bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,10 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           foregroundColor: Colors.white,
         ),
+        bottomAppBarColor: Colors.teal[300],
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.lime[600],
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const Hamburger(),
@@ -53,6 +57,43 @@ class _HamburgerState extends State<Hamburger> {
           ),
           const Header(),
         ],
+      ),
+      // Next line make visible all items that goes behind between the floating
+      // button and bottom navigation bar
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.home),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(45),
+        ),
+        child: Container(
+          color: Colors.black12,
+          child: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            child: Row(
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_alert),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.bookmark),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
